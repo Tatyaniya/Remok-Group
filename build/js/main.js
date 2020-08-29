a0aa689b436eec05e2389aa155ab253bbc9d84ae
Tatyaniya <tatyaniya@bigmir.net>
2020-08-29 22:14:21 +0300

    modal

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,39 @@
 $( function() {
 
-    $('.offer__btn--cost').click( e => {
+
+    /***** Плавная прокрутка *****/
+
+    $('.offer__btn--cost, .menu__list li a').click( e => {
         e.preventDefault();
 		elementClick = $(e.currentTarget).attr("href");
         destination = $(elementClick).offset().top;
-        destination -= 130;
-        $("body,html").animate({scrollTop: destination }, 500);
+        destination -= 116;
+        $("body,html").animate({scrollTop: destination }, 800);
     });
+
+
+    /***** Модалка *****/
+
+    $('.offer__btn--contact, .services__btn').click( e => {
+        e.preventDefault();
+		$('.modal-overlay').fadeIn();
+    });
+
+    $('.modal').click( e => {
+		$('.modal-overlay').fadeOut();
+    });
+
+    /***** Спасибо *****/
+
+    $('.modal__btn').click( e => {
+        e.preventDefault();
+		$('.modal-overlay-thanks').fadeIn();
+    });
+
+    $('.modal-thanks').click( e => {
+		$('.modal-overlay-thanks').fadeOut();
+    });
+
 
      /*** Слайдер c работами ***/
 
@@ -14,6 +41,7 @@ $( function() {
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 1,
+        lazyLoad: 'ondemand',
         prevArrow: '<img src="img/arrow-prev.png" alt="prev" class="works-prev">',
         nextArrow: '<img src="img/arrow-next.png" alt="next" class="works-next">',
     });
@@ -24,7 +52,8 @@ $( function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        adaptiveHeight: true,
+        // variableWidth: true,
+        // adaptiveHeight: true,
         centerMode: true,
         centerPadding: '10px',
         prevArrow: '<img src="img/arrow-prev.png" alt="prev" class="whatSay-prev">',
